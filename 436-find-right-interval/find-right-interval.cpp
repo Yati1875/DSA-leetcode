45c9273d4202c4c94c72529b78373872a9d49312
Yati1875 <yatisharma545@gmail.com>
2025-11-04 21:33:17 +0530

    Time: 15 ms (56.08%) | Memory: 29.3 MB (83.21%) - LeetSync

---
+++ 436-find-right-interval/find-right-interval.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<int> findRightInterval(vector<vector<int>>& intervals) {
+                int n = intervals.size();
+        vector<pair<int, int>> starts; // {start, index}
+        
+        for (int i = 0; i < n; i++) {
+            starts.push_back({intervals[i][0], i});
+        }
+        
+        sort(starts.begin(), starts.end());
+        vector<int> result(n, -1);
+        
+        for (int i = 0; i < n; i++) {
+            int end = intervals[i][1];
+            
+            // Binary search for smallest start >= end
+            int l = 0, r = n - 1, ans = -1;
+            while (l <= r) {
+                int mid = (l + r) / 2;
+                if (starts[mid].first >= end) {
+                    ans = starts[mid].second;
+                    r = mid - 1;
+                } else {
+                    l = mid + 1;
+                }
+            }
+            
+            result[i] = ans;
+        }
+        
+        return result;
+        
+    }
+};
