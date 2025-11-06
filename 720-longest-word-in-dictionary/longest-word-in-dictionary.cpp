d4429031911b81d5d90f17b122addd28edb5a410
Yati1875 <yatisharma545@gmail.com>
2025-11-06 22:27:37 +0530

    Time: 23 ms (61.34%) | Memory: 22.5 MB (78.52%) - LeetSync

---
+++ 720-longest-word-in-dictionary/longest-word-in-dictionary.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string longestWord(vector<string>& words) {
+        sort(words.begin(), words.end());
+        unordered_set<string> built;
+        string result = "";
+        
+        for (string &w : words) {
+            if (w.size() == 1 || built.count(w.substr(0, w.size() - 1))) {
+                built.insert(w);
+                if (w.size() > result.size()) {
+                    result = w;
+                }
+            }
+        }
+        return result;
+        
+    }
+};
