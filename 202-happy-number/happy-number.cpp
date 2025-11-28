2311d46ff51fd1e47eac4f1c4036a654e916876f
Yati1875 <yatisharma545@gmail.com>
2025-11-28 23:52:03 +0530

    Time: 1 ms (31.33%) | Memory: 8.5 MB (6.95%) - LeetSync

---
+++ 202-happy-number/happy-number.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        unordered_set<int> seen;
+
+        while(n != 1 && !seen.count(n)) {
+            seen.insert(n);
+            n = getNext(n);
+        }
+
+        return n == 1;
+    }
+     int getNext(int n) {
+        int sum = 0;
+        while(n > 0) {
+            int d = n % 10;
+            sum += d * d;
+            n /= 10;
+        }
+        return sum;
+    }
+};
